7531a0480f15ac9e95cdeb07d33167cc31e31f21
anton541 <555z.anton@gmail.com>
2023-12-03 21:04:18 +0600

    +main page + assets + tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+
+import 'hover.dart';
 
 void main() {
   runApp(const MyApp());
@@ -70,56 +73,103 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.white,
+        body: DefaultTabController(
+          length: 2,
+          child: SafeArea(
+            left: false,
+            right: false,
+            bottom: false,
+            child: NestedScrollView(
+              headerSliverBuilder: (context, value) {
+                return [
+                SliverAppBar(
+                  backgroundColor: Colors.white,
+                  elevation: 2.0,
+                  pinned: true,
+                  floating: false,
+                  expandedHeight: 300.0,
+                  actions: [
+                    IconButton(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      icon: const Icon(Icons.add_box),
+                      onPressed: () {},
+                    )
+                  ],
+                  leading: IconButton(
+                    padding: const EdgeInsets.only(top: 30.0),
+                    icon: const Icon(Icons.add_box),
+                    onPressed: () {},
+                  ),
+                  bottom: const TabBar(
+                    indicatorColor: Colors.green,
+                    unselectedLabelColor: Colors.grey,
+                    labelColor: Colors.black,
+                    tabs: <Widget>[
+                      Tab(
+                        child: Text(
+                          'Профиль',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      Tab(
+                        child: Text('Настройки',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                            )),
+                      ),
+                    ],
+                  ),
+                  flexibleSpace: FlexibleSpaceBar(
+                    collapseMode: CollapseMode.pin,
+                    background: Center(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            const SizedBox(
+                              height: 36.0,
+                            ),
+                            Image.asset(
+                              'assets/ic_user.webp',
+                              width: 140.0,
+                              height: 140.0,
+                            ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
+                            const Text(
+                              'Екатерина',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24.0),
+                            ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
+                          ]),
+                    ),
+                  ),
+                ),
+                ];
+              },
+              body: TabBarView(
+                children: [
+                  //const ProfileTab(),
+                  Container(
+                    color: const Color.fromARGB(255, 250, 250, 250),
+                    alignment: Alignment.center,
+                    child: const Text('Настройки'),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
